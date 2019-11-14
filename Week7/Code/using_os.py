a9edeae03c6ff77d0703c535366b356a9f60b7b4
Pablo Lechon <plechon@ucm.es>
2019-11-14 13:29:33 +0000

    using_os.py Finished!

--- Week7/Code/using_os.py
+++ Week7/Code/using_os.py
@@ -1,36 +1,60 @@
 """ This is blah blah"""
 
+import subprocess
+import numpy as np
+
 # Use the subprocess.os module to get a list of files and  directories 
 # in your ubuntu home directory 
+# Get the user's home directory.
+home = subprocess.os.path.expanduser("~")
+subprocess.os.listdir(path = home)
 
 # Hint: look in subprocess.os and/or subprocess.os.path and/or 
 # subprocess.os.walk for helpful functions
-
-import subprocess
 
 #################################
 #~Get a list of files and 
 #~directories in your home/ that start with an uppercase 'C'
 
 # Type your code here:
-
-# Get the user's home directory.
-home = subprocess.os.path.expanduser("~")
-
 # Create a list to store the results.
-FilesDirsStartingWithC = []
 
 # Use a for loop to walk through the home directory.
-for (dir, subdir, files) in subprocess.os.walk(home):
-  
+l_root = []
+l_subdir = []
+l_files = []
+for (root, subdir, files) in subprocess.os.walk(home):
+    l_subdir.append(subdir)
+    l_files.append(files)
+
+#Make flat list from list of lists
+l_directories = [j for i in l_subdir for j in i]
+l_files_ = [j for i in l_files for j in i]
+
+#Select only those that start with C
+dir_C = [i for i in l_directories if i.startswith('C')]
+files_C = [i for i in l_files_ if i.startswith('C')]
+
+#Join lists
+DirFiles_C = dir_C + files_C
+np.savetxt('../Results/listC.csv', DirFiles_C, fmt = '%s')
 #################################
 # Get files and directories in your home/ that start with either an 
 # upper or lower case 'C'
 
 # Type your code here:
+#Select only those that start with C or c
 
+dir_C_c = [i for i in l_directories if i.lower().startswith('c')]
+files_C_c = [i for i in l_files_ if i.lower().startswith('c')]
+
+#Join lists
+DirFiles_C_c = dir_C_c + files_C_c
+np.savetxt('../Results/listC_c.csv', DirFiles_C_c, fmt = '%s')
 #################################
 # Get only directories in your home/ that start with either an upper or 
 #~lower case 'C' 
 
 # Type your code here:
+Dir_C_c = dir_C_c
+np.savetxt('../Results/listdirC_c.csv', Dir_C_c, fmt = '%s')
